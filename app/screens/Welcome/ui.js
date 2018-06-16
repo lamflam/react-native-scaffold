50993ba167c3815d982c5fcc866552c9fb3d5c05
Kevin LaFlamme <k@lamfl.am>
2018-06-16 13:56:39 -0400

    Add prettier

--- app/screens/Welcome/ui.js
+++ app/screens/Welcome/ui.js
@@ -9,25 +9,17 @@ import { Platform, Text, View } from 'react-native';
 import { styles } from './styles';
 
 const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu'
 });
 
 export class WelcomeUI extends PureComponent {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit App.js
-                </Text>
-                <Text style={styles.instructions}>
-                    {instructions}
-                </Text>
+                <Text style={styles.welcome}>Welcome to React Native!</Text>
+                <Text style={styles.instructions}>To get started, edit App.js</Text>
+                <Text style={styles.instructions}>{instructions}</Text>
             </View>
         );
     }
