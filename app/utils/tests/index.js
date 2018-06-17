506356fb9e0e3974c94e22931d115cde08b221bf
Kevin LaFlamme <k@lamfl.am>
2018-06-17 15:13:59 -0400

    Default to shallow rendering for snapshot tests

--- app/utils/tests/index.js
+++ app/utils/tests/index.js
@@ -1,6 +1,14 @@
 import testRenderer from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
-export function testSnapshot(component) {
-    const tree = testRenderer.create(component).toJSON();
+export function testSnapshot(component, shallow = true) {
+    let tree;
+    if (shallow) {
+        const renderer = new ShallowRenderer();
+        renderer.render(component);
+        tree = renderer.getRenderOutput();
+    } else {
+        tree = testRenderer.create(component).toJSON();
+    }
     expect(tree).toMatchSnapshot();
 }
